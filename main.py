35fac7dc6d77d0af78c1fffb79945919b4ff01ff
MPace <heympace2@gmail.com>
2018-10-25 19:58:32 -0500

    forms to templates with variable data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cgi      # for auto_escaping
 import os       # for file paths, jinja 
 import jinja2   
 
-# set up path to templates, activate jinja
+# set up file system path to templates, activate jinja
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
@@ -14,66 +14,14 @@ app.config['DEBUG'] = True
 # /// PROCESS /// 
 # 01 - Build form structure
 
-signup_form = """
-<style>
-    .error {{ color: red; }}
-</style>
-<h1>Signup</h1>
-<form method='POST'>
-    <table>
-        <tbody>
-            <tr>
-                <td vAlign="top">
-                    <label for="user_name">Username</label>
-                </td>
-                <td>
-                    <input type="text" name="user_name" value='{user_name}'/>
-                        <p class='error'>{name_error}</p>
-                </td>
-            </tr>
-            <tr>
-                <td vAlign="top">
-                    <label for="user_pass">Password</label>
-                </td>
-                <td>
-                    <input type="password" name="user_pass" value=''/>
-                        <p class='error'>{pass_error}</p>
-                </td>
-            </tr>     
-            <tr>
-                <td vAlign="top">
-                    <label for="user_pass_verify">Verify Password</label>
-                </td>
-                <td>
-                    <input type="password" name="user_pass_verify" value=''/>
-                        <p class='error'>{verify_error}</p>
-                </td>
-            </tr> 
-            <tr>
-                <td vAlign="top">
-                    <label for="user_email">Email (optional)</label>
-                </td>
-                <td>
-                    <input type="text" name="user_email" value='{user_email}'/>
-                        <p class='error'>{email_error}</p>
-                </td>
-            </tr>     
-            <tr>
-                <td colspan="2">
-                    <input type="submit" value="Submit"/>
-                </td>
-            </tr>           
-        </tbody>
-    </table>
-</form>
-"""
 # /// PROCESS /// 
 # 02 - Display form
 
 @app.route('/signup')
 def display_signup_form():
-    return signup_form.format(user_name = '', name_error = '', user_pass = '', pass_error = '', user_pass_verify = '', verify_error = '', user_email = '', email_error = '')
-
+    template = jinja_env.get_template('signup_form.html')
+    return template.render()
+    
 # /// PROCESS /// 
 # 03 - Process form
 # Created two functions to check for empty string, spaces
@@ -147,12 +95,14 @@ def validate_signup():
 
     # CHECK for PASS / Redirect to welcome
     if not u_name_error and not u_pass_error and not u_verify_error and not u_email_error:
-        return redirect('/welcome')
+        user_name = str(user_name)
+        return redirect('/welcome?user_name={0}'.format(user_name))
         # this connects with app.route path below, line 167
 
     # CHECK for ERRORS / activate errors in html
     else: 
-        return signup_form.format(
+        template = jinja_env.get_template('signup_form.html')
+        return template.render(
             name_error=u_name_error,
             pass_error=u_pass_error, 
             verify_error=u_verify_error, 
@@ -166,7 +116,9 @@ def validate_signup():
 # 04 - SET route to WELCOME confirmation page
 @app.route('/welcome')
 def form_validated():
-    return "<h1>Welcome user!</h1>"
+    user_name = request.args.get('user_name')
+    template = jinja_env.get_template('welcome.html')
+    return template.render(user_name=user_name)
 
 app.run()
 
